290e0fa6ac080c1bd9f133462067f97ef7d03012
MesakDuduCoder <duduaoa99@gmail.com>
2023-05-17 23:27:21 +0530

    working with the leaderboard api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import _ from 'lodash';
-import printMe from './print.js';
 import './style.css';
+import { createGame } from './createGame.js';
+import { addScore } from './addScore.js';
+import { getScores } from './getScores.js';
+import { showScores } from './showScores.js';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('div');
+const game = await createGame();
 
-  // Lodash, now imported by this script
-  element.innerHTML = '';
-  element.classList.add('hello');
+const gameId = 'CvCuFW4QPCTH7GbK5ogT';
 
-  btn.innerHTML = '';
-  btn.onclick = printMe;
+const submitScore = document.getElementById('add-score-button');
+submitScore.addEventListener('click', (e) => {
+  e.preventDefault();
+  const score = document.getElementById('score');
+  const name = document.getElementById('name');
+  addScore(name.value, score.value, gameId);
+  name.value = '';
+  score.value = '';
+});
 
-  element.appendChild(btn);
+const refreshScores = document.getElementById('show-scores-button');
+refreshScores.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const updateScores = await getScores(gameId);
+  showScores(updateScores.result);
+});
 
-  return element;
-}
-
-document.body.appendChild(component());
+const scores = await getScores(gameId);
+showScores(scores.result);
